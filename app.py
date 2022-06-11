fa8f2bdf8ffe9f4dcd04d8b828899c831f599975
pokerty <chenhongpo@gmail.com>
2022-06-11 15:23:26 +0800

    v2

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, request, jsonify
-
+# from flask_cors import CORS
 from chat import get_response
 
 app = Flask(__name__)
+# CORS(app)
 
 @app.get("/")
 def index_get():
